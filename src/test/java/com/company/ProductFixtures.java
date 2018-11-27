b9ddff412f0977282e20de8cb412f73491dad5e4
Bora Guven <guvenbe@gmail.com>
2018-11-26 23:30:11 -0500

    Collections complete

---
+++ src/test/java/com/company/ProductFixtures.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.Product;
+
+
+public class ProductFixtures
+{
+    public static Product door = new Product("Wooden Door", 35);
+    public static Product floorPanel = new Product("Floor Panel", 25);
+    public static Product window = new Product("Glass Window", 10);
+}
---
+++ src/test/java/com/company/ReproduceSwapna.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class ReproduceSwapna
+{
+    public static void main(String[] args)
+    {
+        List<String> numbers = new ArrayList<>();
+        numbers.add("1");
+        numbers.add("3");
+        numbers.add("2");
+
+        System.err.println("Removing numbers using for loop");
+        for (String number : numbers)
+        {
+            System.err.println("number = " + number);
+            if (number.equals("2")) {
+                numbers.remove(number);
+            }
+        }
+
+        System.err.println(numbers);
+    }
+}
